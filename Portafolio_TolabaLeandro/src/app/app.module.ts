73f5d8627b111eea2e949670d23ac247ec94bf9d
Leandro <tenok94@gmail.com>
2022-08-15 12:58:20 -0300

    limpiando codigo

--- Portafolio_TolabaLeandro/src/app/app.module.ts
+++ Portafolio_TolabaLeandro/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -21,7 +20,6 @@ import { CopyrightComponent } from './components/copyright/copyright.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedesSocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
