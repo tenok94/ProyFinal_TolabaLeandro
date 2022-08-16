720a39ef5bad81ca4e04c41d2ee8bee29d406525
Leandro <tenok94@gmail.com>
2022-08-16 16:40:55 -0300

    creación de la carpeta backend

---
+++ Portafolio_TolabaLeandro/BackEnd/src/main/java/com/Tolaba/LHT/LhtApplication.java
@@ -0,0 +1,13 @@
+package com.Tolaba.LHT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhtApplication.class, args);
+	}
+
+}
